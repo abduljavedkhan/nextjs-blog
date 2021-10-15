9e585fa39439b157489d4d71a20910c29ba479b7
abduljavedkhan <abduljaved314@gmail.com>
2021-10-15 15:03:38 +0530

    image update

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
-  console.log("res " + JSON.stringify(allPostsData));
   return {
     props: {
       allPostsData,
@@ -21,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, I'm Software Engineer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
